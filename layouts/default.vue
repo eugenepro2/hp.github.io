3d0460869db438295d29e1726f83fb6bd71b5e71
eugenepro2 <eugenepro@icloud.com>
2018-11-07 21:56:48 +0200

    skelet

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-mobile-menu></app-mobile-menu>
+    <nuxt/>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/ui/AppHeader'
+import AppMobileMenu from '../components/ui/AppMobileMenu'
+import AppFooter from '../components/ui/AppFooter'
+export default {
+  components: {
+    AppHeader,
+    AppMobileMenu,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    А это Index Page
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
+
